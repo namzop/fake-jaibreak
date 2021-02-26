13edb3a0b6fd9d7641471665c05f712dc83b0eaf
Namz <namz@namz.gq>
2021-02-26 22:04:54 +0530

    ahh

--- jailbreak/js/lol.js
+++ jailbreak/js/lol.js
@@ -11,7 +11,7 @@ function exploit(){
     alert("Exploiting")
     document.getElementById("statuslol").innerHTML = "Status: Exploiting.";
     setTimeout(() => { document.getElementById("statuslol").innerHTML = "Status: Exploiting.."; }, 2000);
-    setTimeout(() => { document.getElementById("statuslol").innerHTML = "Status: Exploiting..."; }, 2000);
+    setTimeout(() => { document.getElementById("statuslol").innerHTML = "Status: Exploiting..."; }, 4000);
     setTimeout(() => { document.getElementById("statuslol").innerHTML = "Status: Exploited!"; }, 12000);
     setTimeout(() => { alert("Exploited!") }, 12000);
     // document.getElementById("statuslol").innerHTML = "Status: Exploited!";
